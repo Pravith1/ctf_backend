b6031df93196e92ffc14abad7a464c231f8c7734
MUGILAN Y <mugilanmugil29@gmail.com>
2025-10-06 22:08:29 +0530

    added fetchquestions and fetchCategories

--- Routes/submissionRoutes.js
+++ Routes/submissionRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { handleSubmission } = require('../controllers/SubmissionController');
+const { handleSubmission, fetchQuestions, fetchCategories } = require('../controllers/SubmissionController');
 const { verifyToken } = require('../middleware/validate');
 
 // POST /submission - Handle answer submission
 router.post('/', verifyToken, handleSubmission);
+
+// POST /submission/questions - Fetch questions by category (combines year 1 and 2)
+router.post('/questions', verifyToken, fetchQuestions);
+
+// GET /submission/categories - Fetch all categories
+router.get('/categories', verifyToken, fetchCategories);
 
 module.exports = router;
